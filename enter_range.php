bc9fe99bd7cd2b8cafc4919bdfe969911b301f3e
StefanKumarasinghe <89747418+StefanKumarasinghe@users.noreply.github.com>
2023-05-27 11:25:48 +1000

    Update enter_range.php

--- enter_range.php
+++ enter_range.php
@@ -1,103 +1,69 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="script.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+<style>
+    * {
+        font-family: 'Roboto', sans-serif;
+    }
+</style>
+    
+    <!-- Bootstrap CSS -->
+   <!-- Latest compiled and minified CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>    
     <title>Document</title>
 </head>
-<body>
-  <h1>Score Entry</h1>
+<body class="bg-dark text-white">
+    <div class="container-fluid">
+        <div class="col-sm-6 mx-auto mt-5 pt-5">
+  <h1 class="display-6 mt-5">Score Entry</h1>
 
   <?php
-    include "db_connect.inc";
-
+ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    header('Location: archer.html');
+    exit;
+}
     $round = $_POST['round'];
     $archer = $_POST['archer'];
     $equipment = $_POST['equipment'];
-
-    function displayRange()
-    {
-        $ranges = retrieveRange();
-
-        for($i=0; $i<count($ranges); $i++)
-        {
-            echo ("<option value=".$ranges[$i]." selected>".$ranges[$i]."</option>\n");
-        }
-        return;
-    }
-
-    function displayEnds()
-    {
-        $ranges = retrieveRange();
-
-        for($i=0; $i<count($ranges); $i++)
-        {
-            echo ("<option value=".$ranges[$i]." selected>".$ranges[$i]."</option>\n");
-        }
-        return;
-    }
-
+  
   ?>
-
-
-    <form action="insertScore.php" method="post">
+        <form action="insertScore.php" class="mt-2" method="post">
         <?php
         
-            $conn = db();
-            $query = "SELECT firstName, lastName FROM `competitors` WHERE player_id = $archer;";
-            $result = mysqli_query($conn, $query);
-            $output = array();
-
-        if($result)
-        {
-            while($data = mysqli_fetch_array($result))
-            {
-                array_push($output, $data['firstName'], $data['lastName']);
-            }
-        }
-       else
-       {
-        echo "Error";
-       }
-            echo "<p>" . $output[0]." ". $output[1]. ", " . $equipment . "</p>";
-            echo "<p> Round " . $round . "</p>";
+            echo "        <p class='my-2 alert alert-primary fw-bold text-primary'> The archer's name is $archer</p><p class='alert alert-primary my-2 fw-bold text-danger'> The round is  $round</p>       ";
         ?>
-        <input type="hidden" name='round' value=<?php echo $round?>>
-        <input type="hidden" name='archer' value=<?php echo $archer?>>
-        <input type="hidden" name='equipment' value=<?php echo $equipment?>>
+      <input type="hidden" name='round' value=RoundA>
+        <input type="hidden"  name='archer' value=jkdav>
+        <input type="hidden"  name='equipment' value=jsadc>
 
-        <div class="wrapper">
-            <label for="range">Range</label>
-            <select id="range" name="range" required>    
-                 <?PHP displayRange(); ?>
-            </select>
-        </div>
-
-        <div class="wrapper">
-            <label for="end">End</label>
-            <select id="endsNumber" name="end" required>    
-                <option value="1" selected>1</option>
-                <option value="2" selected>2</option>
-                <option value="3" selected>3</option>
-                <option value="4" selected>4</option>
-                <option value="5" selected>5</option>
-            </select>
-        </div>
-
-        <input type="button" id="doneButton" onclick="generateDropdown()" value = "Done">
+        <h3 class="fw-bold mt-5" >Range</p>
+        <input type="text" name="range"  class="form-control my-1 mt-4 p-3 w-100" >
+        <h3  class="fw-bold mt-5"  >Number of ends</h3>
+        <input type="number" name="ends" id="endsNumber"  class="form-control mt-4 my-1 p-3 w-100">
+        <input type="button" class="btn btn-primary p-2 mt-3" id="doneButton" onclick="generateDropdown()" value="Done">
         
         <div id="dropdownContainer"></div>
 
         <div id="submitWrapper" hidden>
-            <input type="submit" value="Submit" >
-
+            <input type="submit" value="Submit" class="btn btn-success">
         </div>
     </form>
-    <?php
-  
-    ?>
+</div>
+</div>
+    <!-- Bootstrap JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
+    <!-- Your custom script -->
+    <script src="script.js"></script>
 </body>
 </html>
